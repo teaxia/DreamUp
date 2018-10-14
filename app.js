b6cabf06e657e5c5bbbfe2141c44dcc2171ef4de
teaxia <my@teaxia.com>
2018-10-14 17:35:05 +0800

    提交到分支

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ App({
     // 登录
     wx.login({
       success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId 
       }
     })
     // 获取用户信息
